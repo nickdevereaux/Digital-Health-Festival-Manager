ddd4ff0e48ca7fb72515cd42368bd6e7ea996ad9
Nick Devereaux <nickjdevereaux@gmail.com>
2026-05-19 14:17:35 +1000

    Configure GitHub Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import vue from '@vitejs/plugin-vue'
 import { resolve } from 'path'
 
 export default defineConfig({
+  base: '/Digital-Health-Festival-Manager/',
   plugins: [vue()],
   resolve: {
     alias: {
